297f16859fda8a1aa68fb66e67a868594dfd4058
lusine <lusine.hovsepyan.y@gmail.com>
2024-12-28 15:15:52 +0400

    new update 12.21.2024

--- app/src/main/java/ru/myitschool/finalproject/MainActivity.java
+++ app/src/main/java/ru/myitschool/finalproject/MainActivity.java
@@ -17,32 +17,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-
-
-        // Handle navigation item clicks
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
-
-            if(item.getItemId() == R.id.nav_home) selectedFragment = new HomeFragment();
-            else if (item.getItemId() == R.id.nav_dashboard) selectedFragment = new HomeFragment();
-            else if (item.getItemId() == R.id.nav_profile) selectedFragment = new ProfileFragment();
-
-            if (selectedFragment != null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, selectedFragment)
-                        .commit();
-            }
-            return true;
-        });
-    }
-
-    private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit();
     }
 }
